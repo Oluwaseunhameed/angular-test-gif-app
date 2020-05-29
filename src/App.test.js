2703a8eb1c0303afa54b6eff138cf5324cc3bb68
Oluwaseunhameed <hameedoluwaseun@gmail.com>
2020-05-29 03:10:56 +0100

    testing for search input and search option results via unit testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,37 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import App from "./App";
+describe("on load page", () => {
+  test("LOGO", () => {
+    const { getByText, getByPlaceholderText, getByTestId } = render(<App />);
+    const logo = getByText(/Gifs-Search/i);
+    const placeholder = getByPlaceholderText(/search gifs[\.+]/i);
+    const SearchIcon = getByText(/Search for a gif/i);
+    const button = getByTestId(/Search/);
+    expect(logo).toBeInTheDocument();
+    expect(placeholder).toBeInTheDocument();
+    expect(SearchIcon).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+  });
+});
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("user can type in search input", () => {
+  test("user can type into search", () => {
+    const { getByTestId } = render(<App />);
+    const input = getByTestId(/searchinput/i);
+    fireEvent.change(input, { target: { value: "a" } });
+    expect(input.value).toBe("a");
+  });
+
+  test("user can see search options", () => {
+    const { getByTestId, getAllByTestId } = render(<App />);
+    const input = getByTestId(/searchinput/i);
+    const button = getByTestId(/Search/);
+    fireEvent.change(input, { target: { value: "a" } });
+    fireEvent.click(button);
+    expect(input.value).toBe("a");
+    setTimeout(() => {
+      expect(getAllByTestId(/gifs/i)).toBeInTheDocument();
+    }, 400);
+  });
 });
